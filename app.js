feacbef6520c429db0db1cd472166134257c05d1
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-13 18:49:19 +0200

    debug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const dayjs        = require ('dayjs');
-dayjs().format();
+
 
 mongoose
   .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
